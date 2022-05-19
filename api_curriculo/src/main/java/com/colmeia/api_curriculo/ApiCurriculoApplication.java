410fe1ecdd8c32a7e301bbb3b17f19ec3f8232c3
DanielCaetano <daniel.caetano@alu.ufc.br>
2022-05-19 18:30:34 -0300

    Criando pastas do projeto

---
+++ api_curriculo/src/main/java/com/colmeia/api_curriculo/ApiCurriculoApplication.java
@@ -0,0 +1,13 @@
+package com.colmeia.api_curriculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCurriculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCurriculoApplication.class, args);
+	}
+
+}
